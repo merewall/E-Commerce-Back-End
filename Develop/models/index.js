f249573337422567463b1275dde8baf40c8462d9
osiyo <merewall@gmail.com>
2021-07-20 11:53:22 -0400

    creating Models for Category, Product, Tag, ProductTag and Category Routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  as: 'Products',
+  through: 'product_tag',
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  as: 'Tags',
+  through: 'product_tag',
+  foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
